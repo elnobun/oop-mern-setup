3108324086a97763a573176239de6d5268dfb3c0
elnobun <elnobun@gmail.com>
2023-05-01 00:51:12 +0100

    updated error handler with status code

--- src/handlers/globalError.handler.ts
+++ src/handlers/globalError.handler.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from "express"
-import APIError from "./apiError.handler";
+import APIError, { HttpStatusCode } from "./apiError.handler";
 
 export const notFound = (req: Request, res: Response, next: NextFunction) => {
-    next(new APIError(`Cannot find ${req.originalUrl} on the server`, 404))
+    next(new APIError(`Cannot find ${req.originalUrl} on the server`, HttpStatusCode.NOT_FOUND))
 }
 
 const validationErrorHandler = (error: any) => {
@@ -10,7 +10,18 @@ const validationErrorHandler = (error: any) => {
     const errorMessages = errors.join('. ')
     const message = `Invalid input data: ${errorMessages}`
 
-    return new APIError(message, 400)
+    return new APIError(message, HttpStatusCode.BAD_REQUEST)
+}
+
+const devErrors = (res: Response, error: APIError) => {
+    if (!error.isOperational) {
+        return res.status(error.statusCode).json({
+            status: error.statusCode,
+            message: error.message,
+            stackTrace: error.stack,
+            error: error
+        })
+    }
 }
 
 const productionError = (res: Response, error: APIError) => {
@@ -20,20 +31,20 @@ const productionError = (res: Response, error: APIError) => {
             message: error.message
         })
     } else {
-        return res.status(500).json({
+        return res.status(HttpStatusCode.INTERNAL_SERVER).json({
             status: 'error',
-            message: 'Something went wrong! pleasse try again.'
+            message: 'Something went wrong! please try again.'
         })
     }
 }
 
 export const errorHandler = (error: APIError, req: Request, res: Response, next: NextFunction) => {
-    error.statusCode = error.statusCode || 500
+    error.statusCode = error.statusCode || HttpStatusCode.INTERNAL_SERVER
     error.status = error.status || 'error'
-    if (error.name === 'ValidationError') {
-        console.log("hello");
-
-        error = validationErrorHandler(error)
+    if (process.env.NODE_ENV === 'development') {
+        devErrors(res, error)
+    } else if (process.env.NODE_ENV === 'production') {
+        if (error.name === 'ValidationError') error = validationErrorHandler(error)
     }
 
     return productionError(res, error)
